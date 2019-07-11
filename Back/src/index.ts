8aeaf102a38cca3f23ed98badd8846eaffec37e5
renan <renanbmx123@gmail.com>
2019-07-11 12:15:16 -0300

    Adicionar sensor no banco... erro no path!!!

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,15 +1,35 @@
 import {connect, Mongoose} from 'mongoose';
+import {plantaModel} from './persistencia/plantaModel';
+import {SensorModel} from './persistencia/sensorModel';
+import {SensorRepositorio} from './persistencia/sensorRepositorio'
+import {plantaRepositorio} from './persistencia/plantaRepositorio';
+import {Sensor} from './entidades/sensor'
 import app from './app';
 
 (async() => {
     try{
-        const url = ('mongodb://localhost:27017/projeto'); 
+        const url = ('mongodb://localhost:27017/projeto');
         await connect(url, { useNewUrlParser: true});
 
         app.listen(app.get('port'));
-        
+
 
     }    catch(error){
         console.log(`Erro: ${error}`);
     }
 })();
+
+async function main() {
+
+    try {
+        console.log('Criar Sensor...');
+        let sns: Sensor = {tipo: 'banana'}
+        let sensor1 = await SensorRepositorio.criaSensor(sns);
+        console.log(sensor1);
+
+     } catch (erro) {
+         console.log(`Erro: ${erro.message}`);
+    }
+}
+
+main();
