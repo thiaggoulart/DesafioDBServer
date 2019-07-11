563759a044405ba7ef12c37a3bc57c24d8e8d66d
unknown <thiaggoulart@gmail.com>
2019-07-11 14:02:48 -0300

    corrige bugs 2

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,12 +1,7 @@
-import {connect, Mongoose} from 'mongoose';
-import {plantaModel} from './persistencia/plantaModel';
-import {SensorModel} from './persistencia/sensorModel';
+import {connect} from 'mongoose';
 import {SensorRepositorio} from './persistencia/sensorRepositorio'
 import {plantaRepositorio} from './persistencia/plantaRepositorio';
-import {Sensor} from './entidades/sensor'
 import app from './app';
-import { SensorRepositorio } from './persistencia/sensorRepositorio';
-import { plantaRepositorio } from './persistencia/plantaRepositorio';
 
 (async() => {
     try{
@@ -21,31 +16,16 @@ import { plantaRepositorio } from './persistencia/plantaRepositorio';
         });*/
 
         //Insere Planta
-        /*let sensorID = await SensorRepositorio.criaSensor({
-            tipo: 'Banana'
+        let sensorID = await SensorRepositorio.criaSensor({
+            tipo: 'Violeta'
         });
 
         await plantaRepositorio.criaPlanta({
             sensor: sensorID,
             umidade: 300
-        });*/
+        });
 
     }    catch(error){
         console.log(`Erro: ${error}`);
     }
 })();
-
-async function main() {
-
-    try {
-        console.log('Criar Sensor...');
-        let sns: Sensor = {tipo: 'banana'}
-        let sensor1 = await SensorRepositorio.criaSensor(sns);
-        console.log(sensor1);
-
-     } catch (erro) {
-         console.log(`Erro: ${erro.message}`);
-    }
-}
-
-main();
