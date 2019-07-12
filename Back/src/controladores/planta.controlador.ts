7aabcfd066f061e0ef7d6202b514a6dfeba0cbc4
unknown <thiaggoulart@gmail.com>
2019-07-12 10:53:20 -0300

    verifica umidade na hora de cadastrar

--- Back/src/controladores/planta.controlador.ts
+++ Back/src/controladores/planta.controlador.ts
@@ -4,10 +4,57 @@ import { SensorRepositorio } from '../persistencia/sensorRepositorio';
 
 export async function postPlanta(req: Request, res: Response, next: NextFunction){
     try {
-        console.log('chegou')
         const sensor = await SensorRepositorio.buscaSensorId(req.body.sensor);
         if(sensor !== null){
             const planta = await plantaRepositorio.criaPlanta({umidade: req.body.valorLeitura, sensor: sensor});
+
+            switch (sensor.tipo){
+                case 'morango':
+                    if(planta.umidade > 80){
+                        res.send(`Muita umidade no sensor ${sensor}`);
+                    }
+                    else if(planta.umidade < 60){
+                        res.send(`Pouca umidade no sensor ${sensor}`);
+                    }
+                break;
+
+                case 'violeta':
+                    if(planta.umidade > 80){
+                        res.send(`Muita umidade no sensor ${sensor}`);
+                    }
+                    else if(planta.umidade < 40){
+                        res.send(`Pouca umidade no sensor ${sensor}`);
+                    }
+                break;
+
+                case 'lavanda':
+                    if(planta.umidade > 30){
+                        res.send(`Muita umidade no sensor ${sensor}`);
+                    }
+                    else if(planta.umidade < 20){
+                        res.send(`Pouca umidade no sensor ${sensor}`);
+                    }
+                break;
+
+                case 'cacto':
+                    if(planta.umidade > 50){
+                        res.send(`Muita umidade no sensor ${sensor}`);
+                    }
+                    else if(planta.umidade < 20){
+                        res.send(`Pouca umidade no sensor ${sensor}`);
+                    }
+                break;
+
+                case 'cogumelo':
+                    if(planta.umidade > 90){
+                        res.send(`Muita umidade no sensor ${sensor}`);
+                    }
+                    else  if(planta.umidade < 80){
+                        res.send(`Pouca umidade no sensor ${sensor}`);
+                    }
+                break;
+            }
+
             res.json(planta);
         } else {
             res.status(404).end();
