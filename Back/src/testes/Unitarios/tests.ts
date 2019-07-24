929fa1328afd0dca700ab198119e25b746ce088c
Thiago Goulart <thiaggoulart@gmail.com>
2019-07-24 15:23:59 -0300

    Organização testes

--- Back/src/testes/Unitarios/tests.ts
+++ Back/src/testes/Unitarios/tests.ts
@@ -1,17 +1,18 @@
 import { Sensor } from "../../entidades/sensor";
 import { Planta } from "../../entidades/planta";
 import {plantaRepositorio} from "../../persistencia/plantaRepositorio";
-
-const sensor: Sensor = {
-  tipo: 'morango',
-  status: 'top'
-}
-const planta: Planta = {
-  sensor: sensor,
-  umidade: 80
-}
+import { SensorRepositorio } from "../../persistencia/sensorRepositorio";
 
 describe('Testing if all functions are working', () => {
+
+  const sensor: Sensor = {
+    tipo: 'morango',
+    status: 'top'
+  }
+  const planta: Planta = {
+    sensor: sensor,
+    umidade: 80
+  }
 
   it('Should create a plant.', async () => {
 
@@ -72,5 +73,53 @@ describe('Testing if all functions are working', () => {
     expect(SPY).toHaveBeenCalledTimes(1);
     expect(result).toEqual([planta]);
   });
+
+  it('Should create a sensor.', async () => {
+
+    const SPY = jest.fn(() => sensor);
+    jest
+        .spyOn(SensorRepositorio, 'criaSensor')
+        .mockImplementation(async ()=> SPY());
+
+    let result = await SensorRepositorio.criaSensor(sensor);
+    expect(SPY).toHaveBeenCalledTimes(1);
+    expect(result).toBe(sensor);
+  });
+
+    it('Should find a sensor.', async () => {
+
+    const SPY = jest.fn(() => sensor);
+    jest
+        .spyOn(SensorRepositorio, 'buscaSensorId')
+        .mockImplementation(async ()=> SPY());
+
+    let result = await SensorRepositorio.buscaSensorId("teste");
+    expect(SPY).toHaveBeenCalledTimes(1);
+    expect(result).toBe(sensor);
+    });
+
+    it('Should find sensors by type.', async () => {
+
+        const SPY = jest.fn(() => [sensor]);
+        jest
+            .spyOn(SensorRepositorio, 'buscarPorTipo')
+            .mockImplementation(async ()=> SPY());
+    
+        let result = await SensorRepositorio.buscarPorTipo("morango");
+        expect(SPY).toHaveBeenCalledTimes(1);
+        expect(result).toEqual([sensor]);
+    });
+
+    it('Should find all sensors.', async () => {
+
+        const SPY = jest.fn(() => [sensor]);
+        jest
+            .spyOn(SensorRepositorio, 'buscaTodos')
+            .mockImplementation(async ()=> SPY());
+    
+        let result = await SensorRepositorio.buscaTodos();
+        expect(SPY).toHaveBeenCalledTimes(1);
+        expect(result).toEqual([sensor]);
+    });
 
 });
